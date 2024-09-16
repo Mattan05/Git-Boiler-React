096dd38117c2d79d662ec8295744304cfe0c2d3a
Mattis Bertilsson <mattisbertilsson5@gmail.com>
2024-09-16 09:49:50 +0200

    boiler plate parcel react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+    return ( 
+        <>
+            <h2>React App With Parcel</h2>
+            <h3>Not So Hard as it Seems...</h3>
+            <p><i>Final test</i></p>
+        </>
+     );
+}
+
+export default App;
